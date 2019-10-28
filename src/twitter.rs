c02eafe3174114593e790c14d6ff8440b58fde96
Afnan Enayet <afnanenayet@users.noreply.github.com>
2019-10-28 13:43:16 -0700

    Delete empty file

--- src/twitter.rs
+++
@@ -1,5 +0,0 @@
-/// Type-safe and error'd wrappers around the Twitter API. This module creates methods that perform
-/// the acions we want in Twitter's API without in a manner that's more safe than writing raw
-/// requests to the API. 
-
-
